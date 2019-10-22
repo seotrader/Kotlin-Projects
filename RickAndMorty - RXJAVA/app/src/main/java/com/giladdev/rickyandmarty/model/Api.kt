c6ded82968793d9cd6888f09caa77e5616f24a32
seotrader <gilad.fastseo@gmail.com>
2019-10-22 12:41:45 -0400

    delete

--- RickAndMorty - RXJAVA/app/src/main/java/com/giladdev/rickyandmarty/model/Api.kt
+++
@@ -1,9 +0,0 @@
-package com.giladdev.rickyandmarty.model
-
-import io.reactivex.Single
-import retrofit2.http.*
-
-interface Api {
-    @GET("character/")
-    fun GetCharecters(@Query("page") page : String?) : Single<CharacterList>
-    }
