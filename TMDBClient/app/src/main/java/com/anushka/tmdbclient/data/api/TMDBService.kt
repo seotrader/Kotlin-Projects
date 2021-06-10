d917512ae8b445df31fa587a91826d71a9da62b9
seotrader <gilad.fastseo@gmail.com>
2021-06-09 22:11:26 -0400

    TMD Client Update

---
+++ TMDBClient/app/src/main/java/com/anushka/tmdbclient/data/api/TMDBService.kt
@@ -0,0 +1,25 @@
+package com.anushka.tmdbclient.data.api
+
+import com.anushka.tmdbclient.data.model.artist.ArtistList
+import com.anushka.tmdbclient.data.model.movie.MovieList
+import com.anushka.tmdbclient.data.model.tvshow.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+    @GET("movie/popular")
+    suspend fun getPopularMovie(
+        @Query("api_key") apiKey: String
+    ): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(
+        @Query("api_key") apiKey: String
+    ): Response<TvShowList>
+
+    @GET("person/populars")
+    suspend fun getPopularArtists(
+        @Query("api_key") apiKey: String
+    ): Response<ArtistList>
+}
---
+++ TMDBClient/app/src/main/java/com/anushka/tmdbclient/data/model/tvshow/TvShow.kt
@@ -0,0 +1,22 @@
+package com.anushka.tmdbclient.data.model.tvshow
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "popular_tv_shows")
+data class TvShow(
+    @PrimaryKey
+    @SerializedName("first_air_date")
+    val firstAirDate: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("poster_path")
+    val posterPath: String
+
+)
